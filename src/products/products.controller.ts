b33252954b5c7d841e68d9583796e70c27fa6fcc
marvin espira <marvinespira@adalabsafrica.com>
2021-10-10 00:43:18 +0300

    project boilerplate + updates

---
+++ src/products/products.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Post, Body } from '@nestjs/common';
+
+import { ProductsService } from './products.service';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Post()
+  addProduct(
+    // @Body() completeBody: {title: string, description: string, price: number}
+    @Body('title') prodTitle: string,
+    @Body('description') prodDesc: string,
+    @Body('price') prodPrice: number,
+  ): any {
+    const generatedId = this.productsService.insertProduct(
+      prodTitle,
+      prodDesc,
+      prodPrice,
+    );
+    return { id: generatedId };
+  }
+}
