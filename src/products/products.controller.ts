6ae21f22dc4ab61c2455c536436c0e787dfde4e9
marvin espira <espiramarvin@gmail.com>
2022-10-28 15:00:30 +0300

    fix: formatting

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Post, Body, Get, Param, Patch , Delete } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Param,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 
 import { ProductsService } from './products.service';
 
